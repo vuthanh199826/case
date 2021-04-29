6fecd5624a365fde19428b673d349dc909522417
vuthanh199826 <vuthanh199825@gmail.com>
2021-04-29 16:50:16 +0700

    fix

--- Tank.js
+++ Tank.js
@@ -8,7 +8,7 @@ function clearAll() {
 
 function Tank(x, y, id) {
     this.power = 1;
-    this.blood = 200;
+    this.blood = 1000;
     this.score = 0;
     this.bullets = [];
     this.x = x;
@@ -36,7 +36,7 @@ function Tank(x, y, id) {
         } else {
             ctx.fillStyle = "rgb(164,10,10)";
         }
-        ctx.fillRect(this.x, this.y -15, this.blood * 70 / 200, 10);
+        ctx.fillRect(this.x, this.y -15, this.blood * 70 / 1000, 10);
     }
     this.moveUp = function () {
         this.id = "up";
